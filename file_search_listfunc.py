34a5d9f93e1063d13aa34367923552b5d963fbaf
sindhura <sindhurabuggaveeti@gmail.com>
2021-03-29 19:39:09 -0400

    Added cal list generation code with get list function

--- file_search_listfunc.py
+++ file_search_listfunc.py
@@ -3,14 +3,12 @@ import glob
 import re
 import csv
 
-list_new = []
-list_old = []
-list_final = []
+
 # root_dir needs a trailing slash (i.e. /root/dir/)
 
-def GetCalList():
-
-    for filename in glob.iglob(r'C:\Users\sindh\Documents\software\VECR_valve_new\**\vec?k???.txt', recursive = True):
+def get_calibration_list(path):
+    list_out = []
+    for filename in glob.iglob(path, recursive=True):
         print(filename)
         pattern1 = re.compile(r'([\w]+)[ ]+([\w]+)[ ]+=[ ]+(\{([^{]+)\}|\w+)')
         pattern2 = re.compile(r'(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)')
@@ -22,9 +20,10 @@ def GetCalList():
                 matches1 = pattern1.finditer(separate_statement)
                 matches2 = pattern2.finditer(separate_statement)
 
-                list1 = []; there = False
+                list1 = []
+                there = False
                 for match1 in matches1:
-                    #print(match1.group(2))
+                    # print(match1.group(2))
                     value = match1.group(3)
                     list1 = [match1.group(1), match1.group(2), " ".join(value.split())]
 
@@ -32,53 +31,27 @@ def GetCalList():
                     comments = match2.group(1)
                     comments = comments.replace('/* Object: ', '')
                     comments = comments.replace('*/', '')
-                    comments = comments.replace('\n  ', '')
+                    #comments = comments.replace('\n  ', '')
                     there = True
-                    list1.append(comments)
+                    list1.append(" ".join(comments.split()))
                     list1.append('Engine Thermal Management TRB')
 
                 if there == False and len(list1) != 0:
                     list1.append('')
                     list1.append('Engine Thermal Management TRB')
 
-                list_new.append(list1)
-                list_new = [x for x in list_new if x != []]
-    return list
+                list_out.append(list1)
+                list_out = [x for x in list_out if x != []]
+    return list_out
 
-for filename in glob.iglob(r'C:\Users\sindh\Documents\software\VECR_valve_old\**\vec?k???.txt', recursive = True):
-    print(filename)
-    pattern1 = re.compile(r'([\w]+)[ ]+([\w]+)[ ]+=[ ]+(\{([^{]+)\}|\w+)')
-    pattern2 = re.compile(r'(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)')
-    with open(filename, 'r') as f:
-        contents = f.read()
-        separate_statements = contents.split(';')
-        for separate_statement in separate_statements:
 
-            matches1 = pattern1.finditer(separate_statement)
-            matches2 = pattern2.finditer(separate_statement)
-
-            list1 = []; there = False
-            for match1 in matches1:
-                #print(match1.group(2))
-                value = match1.group(3)
-                list1 = [match1.group(1), match1.group(2), " ".join(value.split())]
-
-            for match2 in matches2:
-                comments = match2.group(1)
-                comments = comments.replace('/* Object: ', '')
-                comments = comments.replace('*/', '')
-                comments = comments.replace('\n  ', '')
-                there = True
-                list1.append(comments)
-                list1.append('Engine Thermal Management TRB')
-            if there == False and len(list1) != 0:
-                list1.append('')
-                list1.append('Engine Thermal Management TRB')
-
-            list_old.append(list1)
-            list_old = filter(None, list_old)
-            list_old = [x for x in list_old if x != []]
-
+list_new = []
+list_old = []
+list_final = []
+path_new = r'C:\Users\sindh\Documents\software\VECR_valve_new\**\vec?k???.txt'
+path_old = r'C:\Users\sindh\Documents\software\VECR_valve_old\**\vec?k???.txt'
+list_new = get_calibration_list(path_new)
+list_old = get_calibration_list(path_old)
 print(list_old)
 print(len(list_old[0]))
 print(list_new)
@@ -94,7 +67,7 @@ added_value_columns_old = [line[2] for line in list_old if line]
 added_description_columns_old = [line[3] for line in list_old if line]
 
 for list_elem in list_new:
-    if not(any([list_elem[1] == item for item in added_name_columns_old])):
+    if not (any([list_elem[1] == item for item in added_name_columns_old])):
         status = 'Add'
         list3 = [status] + list_elem
         list_final.append(list3)
@@ -102,9 +75,9 @@ for list_elem in list_new:
 for list_elem in list_old:
     if any([list_elem[1] == item for item in added_name_columns_new]):
         index = added_name_columns_new.index(list_elem[1])
-        if not(list_elem[0] == added_datatype_columns_new[index] and
-               list_elem[2] == added_value_columns_new[index] and
-               list_elem[3] == added_description_columns_new[index]):
+        if not (list_elem[0] == added_datatype_columns_new[index] and
+                list_elem[2] == added_value_columns_new[index] and
+                list_elem[3] == added_description_columns_new[index]):
             status = 'modify'
             list3 = [status] + list_elem
             list_final.append(list3)
